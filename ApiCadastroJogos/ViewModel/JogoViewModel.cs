41dd31157760806acd65a5c831795918f75f197a
Anderson Viana <valladau2@gmail.com>
2021-08-31 10:23:15 -0300

    Criando a classe ViewModel

---
+++ ApiCadastroJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ApiCadastroJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public string Produtora { get; set; }
+        
+        public double Preco { get; set; }
+    }
+}
