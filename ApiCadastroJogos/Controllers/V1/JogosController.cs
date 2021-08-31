50c8329edbc0848e924430fc0bdf08c5222057fc
Anderson Viana <valladau2@gmail.com>
2021-08-31 09:57:13 -0300

    Criando a classe Controller com os verbos HTTP

---
+++ ApiCadastroJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCadastroJogos.Controllers.V1
+{
+    [ApiController]
+    [Route("ap/V1/[controller]")]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+    }  
+}
