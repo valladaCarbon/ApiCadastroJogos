f0d72d2464451dbb15300fb65bf6bfc14e355f2a
Anderson Viana <valladau2@gmail.com>
2021-08-31 10:22:02 -0300

    Modificando a classe Controller, incluindo os retornos de Input e View

--- ApiCadastroJogos/Controllers/V1/JogosController.cs
+++ ApiCadastroJogos/Controllers/V1/JogosController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ApiCadastroJogos.InputModel;
+using ApiCadastroJogos.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCadastroJogos.Controllers.V1
@@ -10,25 +12,25 @@ namespace ApiCadastroJogos.Controllers.V1
     public class JogosController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<object>>> Obter()
+        public async Task<ActionResult<List<JogoViewModel>>> Obter()
         {
             return Ok();
         }
 
         [HttpGet("{idJogo:guid}")]
-        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        public async Task<ActionResult<List<JogoViewModel>>> Obter(Guid idJogo)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo)
         {
             return Ok();
         }
 
         [HttpPut("{idJogo:guid}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo)
         {
             return Ok();
         }
